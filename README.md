624d59a2d22b9750c64f01c32564eb5df3b96b96
herbherbherb <rwang67@illinois.edu>
2017-11-25 14:55:10 -0600

    revised

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python-Chat-App
+# Create by Herbert Wang (rwang67@illinois.edu)
+# using Flask-socketio with eventlet to allow mixed content http + wss
--- backup.py
+++ backup.py
@@ -12,9 +12,8 @@ import eventlet.wsgi
 eventlet.monkey_patch()
 
 app = Flask(__name__)
-# sio = SocketIO( app, async_handlers=True)
-
-sio = socketio.Server()
+# sio = socketio.Server()
+sio = SocketIO( app, async_handlers=True)
 
 # app = Flask(__name__)
 app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
@@ -33,63 +32,61 @@ def hello():
 #     return flask.Response("foo" * 10, mimetype='text/plain')
 
 @sio.on('connect')
-def on_connect(sid, environ):
-	print("connect ", sid) 
+def on_connect():
+	print('Entered!!!')   
 	sio.emit('hello', 'yes')
 
 @sio.on('disconnect')
-def on_disconnect(sid):
+def on_disconnect(data):
 	# username = data['username']
 	# domain = data['domain_name']
 	print(' left the game!')
 	# leave_room(domain)
 	# send(username + ' has left your domain', room=domain)
-	# sio.emit('leave domain', 'You left ' + domain + 'successfully!', room=sid)
+	# emit('leave domain', 'You left ' + domain + 'successfully!', room=request.sid)
 	# for x in users:
 	# 	if x == username:
 	# 		users.remove(x)
 	# sio.emit('get users', users, room=domain)
 
 @sio.on( 'my event' )
-def handle_my_custom_event(sid, json):
-  print( 'recived my event: ' + str( json ) ) #user is the sid
-  print ('current user id is: ' + sid)
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) ) #user is the request.sid
+  print ('current user id is: ' + request.sid)
   sio.emit( 'my response', json, callback=messageRecived )
 
 @sio.on('join')
-def on_join(sid, data):
-	currentsid = sid
+def on_join(data):
 	username = data['username']
 	domain = data['domain_name']
 	join_room(domain)
 	send(username + ' has entered your domain', room=domain)
-	sio.emit('enter domain', 'You entered ' + domain + 'successfully!', room=sid)
+	emit('enter domain', 'You entered ' + domain + 'successfully!', room=request.sid)
 
 
 @sio.on('leave')	
-def on_leave(sid, data):
+def on_leave(data):
 	username = data['username']
 	domain = data['domain_name']
 	print(username + ' left the game!')
 	# leave_room(domain)
 	# send(username + ' has left your domain', room=domain)
-	# sio.emit('leave domain', 'You left ' + domain + 'successfully!', room=sid)
+	# emit('leave domain', 'You left ' + domain + 'successfully!', room=request.sid)
 	for x in users:
 		if x == username:
 			users.remove(x)
 	sio.emit('get users', users, room=domain)
 
 @sio.on( 'change domain' )
-def change_domain(sid, data): 
-	currentsid = sid
+def change_domain(data): 
 	username = data['username']	
 	newdomain = data['domain_name']
-	olddomain = dic[sid]
-	dic[sid] = newdomain
+	olddomain = dic[request.sid]
+	dic[request.sid] = newdomain
 	leave_room(olddomain)
 	join_room(newdomain)
 	send(username + ' has entered your domain', room=newdomain)
-	sio.emit('new domain', 'You entered ' + newdomain + ' successfully!', room=sid)
+	emit('new domain', 'You entered ' + newdomain + ' successfully!', room=request.sid)
 
 def print_url(r, *args, **kwargs):
 	print('Query recieved!')
@@ -103,61 +100,52 @@ def calllib(domain, message):
 	return r
 
 # @sio.on('send message')
-# def send_message(sid, data):
+# def send_message(data):
 # 	username = data['username']
 # 	message = data['message']
 # 	message = urlparse(message)
-# 	domain = dic[sid]
+# 	domain = dic[request.sid]
 # 	payload = {'url': domain, 'query': message}
 # 	r = requests.get(url, hooks={'response': print_url}, params=payload)
-# 	sio.emit('new message', {'msg': r.text, 'users': username}, room=domain)
+# 	emit('new message', {'msg': r.text, 'users': username}, room=domain)
 
 @sio.on('send message by desc')
-def send_message_by_desc(sid, data):
+def send_message_by_desc(data):
 	username = data['username']
 	message = data['message']
 	message = urlparse(message)
-	domain = dic[sid]
+	domain = dic[request.sid]
 	payload_desc = {'url': domain, 'querydesc': message}
 	r = requests.get(url, hooks={'response': print_url_desc}, params=payload_desc)
-	for key in dic:
-		if dic[key] == domain:
-			sio.emit('new message', {'msg': r.text, 'users': username}, room=key)
-
-	# sio.emit('new message', {'msg': r.text, 'users': username}, room=domain)
+	emit('new message', {'msg': r.text, 'users': username}, room=domain)
 
 @sio.on('send message')
-def send_message(sid, data):
+def send_message(data):
 	username = data['username']
 	message = data['message']
-	domain = dic[sid]
-	for key in dic:
-		if dic[key] == domain:
-			sio.emit('new message', {'msg': message, 'users': username}, room=key)
-	
+	domain = dic[request.sid]
+	emit('new message', {'msg': message, 'users': username}, room=domain)
+
 @sio.on('new user')
-def new_user(sid, data):
-	currentsid = sid
+def new_user(data):
 	username = data['username']	
 	users.append(username);
 	domain = data['domain_name']
-	dic[sid] = domain
-	print(sid);
-	# join_room(domain)
-	# send(username + ' has entered your domain', room=domain)
-	# sio.emit('get users', users, room=domain)
-	for key in dic:
-		if dic[key] == domain:
-			sio.emit('get users', users, room=key)
-	# sio.emit('get users', users, room=sid)
+	dic[request.sid] = domain
+	print(request.sid);
+	join_room(domain)
+	send(username + ' has entered your domain', room=domain)
+	# emit('get domains', domain, room=request.sid)  don't need this anymore!!
+	emit('get users', users, room=domain)
+	emit('get users', users, room=request.sid)
 
 
 if __name__ == '__main__':
-	app = socketio.Middleware(sio, app)
+	# app = socketio.Middleware(sio, app)
     # deploy as an eventlet WSGI server
 	# eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 5353)), app)
-	# eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('127.0.0.1', 5353)), certfile='cert.crt',keyfile='private.key',server_side=True), app)
-	eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5353)), app)
-	eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('0.0.0.0', 5353)), certfile='cert.crt',keyfile='private.key',server_side=True), app)
+	# wsgi.server(eventlet.wrap_ssl(eventlet.listen(('', 5353)), certfile='cert.crt',keyfile='private.key',server_side=True), app)
 	# sio.run(app, debug=True, host="0.0.0.0", port=5353)
-	# sio.run(app, debug=True, port=5353)
+	sio.run(app, debug=True, port=5353)
+	# wsgi.server(eventlet.wrap_ssl(eventlet.listen(('127.0.0.1', 5353)), certfile='cert.crt',keyfile='private.key',server_side=True), app)
+
