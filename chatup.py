25e1c0b20921c35295d450007b20411c4beea8c6
herbherbherb <rwang67@illinois.edu>
2017-11-25 09:59:33 -0600

    revised

--- chatup.py
+++ chatup.py
@@ -138,5 +138,5 @@ def new_user(data):
 
 
 if __name__ == '__main__':
-	# socketio.run(app, debug=True, host="0.0.0.0", port=5353)
-	socketio.run(app, debug=True, port=5353)
+	socketio.run(app, debug=True, host="0.0.0.0", port=5353)
+	# socketio.run(app, debug=True, port=5353)
